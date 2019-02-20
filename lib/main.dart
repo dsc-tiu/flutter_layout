0cddf463f25c2105bc7953dc4ead92059564d649
ATRI DAS <babinsamrat123@gmail.com>
2019-02-20 20:08:13 +0530

    uploaded by samrat19

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: LayoutApp(),
+    debugShowCheckedModeBanner: false,
+    ),
+);
+
+class LayoutApp extends StatefulWidget {
+  @override
+  _LayoutAppState createState() => _LayoutAppState();
+}
+
+class _LayoutAppState extends State<LayoutApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
