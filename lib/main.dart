986b63953df955820cf6d30cfb103bc283e1490a
ATRI DAS <babinsamrat123@gmail.com>
2019-02-21 00:10:20 +0530

    upgrade6

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,42 @@ class _LayoutAppState extends State<LayoutApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.redAccent,
         title: Text("Layout Learning"),
       ),
       drawer: Drawer(
         //todo
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("DSC TIU"),
+              accountEmail: Text("dsctechnoindiauniversity@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                child: Text("D",style:TextStyle(fontSize:30.0,)),
+                backgroundColor: Colors.red,
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  child: Text("A",style: TextStyle(fontSize: 15.0),),
+                  backgroundColor: Colors.pink,
+                ),
+                CircleAvatar(
+                  child: Text("B",style: TextStyle(fontSize: 15.0),),
+                  backgroundColor: Colors.purple,
+                )
+              ],
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle,size: 40.0,color: Colors.red,),
+              title: Text("Primary"),
+            ),
+          ],
+        ),
       ),
       body: ListView(
         children: <Widget>[
           Identity(),
-          Identity()
+          Identity(),
         ],
       ),
     );
@@ -42,7 +69,8 @@ class Identity extends StatelessWidget {
             children: <Widget>[
               Container(
                 height: 50.0,
-                width: 100.0,
+                width: 50.0,
+                padding: EdgeInsets.only(left: 10.0),
                 child: Icon(
                     Icons.account_circle,
                     color: Colors.redAccent,
@@ -51,7 +79,7 @@ class Identity extends StatelessWidget {
               ),
               Container(
                // color: Colors.black,
-                padding: EdgeInsets.all(20.0),
+                padding: EdgeInsets.fromLTRB(20.0, 20.0,20.0,20.0),
                 height: 100.0,
                 width: 200.0,
                 child: ListView(
