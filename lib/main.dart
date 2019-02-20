09d4f5fc0f1b6ba6257ef451ce232f73251645c1
ATRI DAS <babinsamrat123@gmail.com>
2019-02-20 20:41:45 +0530

    upgrade4

--- lib/main.dart
+++ lib/main.dart
@@ -23,22 +23,34 @@ class _LayoutAppState extends State<LayoutApp> {
         //todo
       ),
       body:Container(
-        height: 200.0,
-        child: Row(
-          children: <Widget>[
-            Container(
-              height: 200.0,
-              width: 100.0,
-              child: Center(
+          height: 100.0,
+          //color: Colors.brown,
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 50.0,
+                width: 100.0,
                 child: Icon(
-                  Icons.account_circle,
-                  color: Colors.redAccent,
-                ),
+                    Icons.account_circle,
+                    color: Colors.redAccent,
+                    size: 50.0,
+                  ),
               ),
-            ),
-          ],
+              Container(
+               // color: Colors.black,
+                padding: EdgeInsets.all(20.0),
+                height: 100.0,
+                width: 150.0,
+                child: ListView(
+                  children: <Widget>[
+                    Text("Name",style: TextStyle(fontSize: 25.0),),
+                    Text("email Id",style: TextStyle(fontSize: 25.0),)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
     );
   }
 }
