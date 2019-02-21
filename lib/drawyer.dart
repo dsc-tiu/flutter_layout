9fa58377d8cc38446b6c0e8681c8032e66ad9a41
ATRI DAS <babinsamrat123@gmail.com>
2019-02-22 02:09:34 +0530

    update_drawyer

--- lib/drawyer.dart
+++ lib/drawyer.dart
@@ -35,8 +35,12 @@ class Account extends StatelessWidget {
         ),
         ListTile(
           leading: Icon(Icons.inbox,color: Colors.black54,),
-          title: Text("Updates",style: TextStyle(color: Colors.black54),),
+          title: Text("Forum",style: TextStyle(color: Colors.black54),),
         ),
+        Divider(height:20.0),
+        Text("  Gmail",style: TextStyle(color: Colors.black,fontSize: 20.0),),
+        Divider(height: 20.0,),
+        Divider(height:20.0),
       ],
     );
   }
