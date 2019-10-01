60d9c7b07b75875b0c31f1e0b210e253de40b572
Shoili Chowdhury <schowdhury.shoili@gmail.com>
2019-10-01 21:59:40 +0530

    Updated
    UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:layout_learning/drawyer.dart';
 
 void main() => runApp(
   MaterialApp(
     home: LayoutApp(),
     debugShowCheckedModeBanner: false,
-    ),
+  ),
 );
 
 class LayoutApp extends StatefulWidget {
@@ -22,49 +21,76 @@ class _LayoutAppState extends State<LayoutApp> {
         title: Text("Layout Learning"),
       ),
       drawer: Drawer(
-        child: Account(),
+        //todo
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("DSC TIU"),
+              accountEmail: Text("dsctechnoindiauniversity@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                child: Text("D",style:TextStyle(fontSize:30.0,)),
+                backgroundColor: Colors.red,
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  child: Text("A",style: TextStyle(fontSize: 15.0),),
+                  backgroundColor: Colors.pink,
+                ),
+                CircleAvatar(
+                  child: Text("B",style: TextStyle(fontSize: 15.0),),
+                  backgroundColor: Colors.purple,
+                )
+              ],
+            ),
+            Category(type: "Primary", iconImage: Icons.computer),
+            Category(type: "Social", iconImage: Icons.person),
+            Category(type: "Promotions", iconImage: Icons.label_outline),
+            Category(type: "Updates", iconImage: Icons.info_outline),
+            Category(type: "Forums", iconImage: Icons.message)
+          ],
+        ),
       ),
       body: ListView(
         children: <Widget>[
-         Client(),
-         Client(),
-         Client(),
+          Identity(),
+          Identity(),
         ],
       ),
     );
   }
 }
 
-class Client extends StatelessWidget {
+class Identity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-          height: 100.0,
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 50.0,
-                width: 50.0,
-                padding: EdgeInsets.only(left: 10.0),
-                child: Icon(
-                    Icons.account_circle,
-                    color: Colors.redAccent,
-                    size: 50.0,
-                  ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(20.0, 20.0,20.0,20.0),
-                height: 100.0,
-                width: 200.0,
-                child: ListView(
-                  children: <Widget>[
-                    Text("client name Title",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
-                    Text("client email Id",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-              )
-            ],
+    return Card(
+        child: ListTile(
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.redAccent,
+            size: 50.0,
           ),
-        );
+          title: Text("Client Email",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold)),
+          subtitle: Text("Client Name",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold)),
+        )
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String type;
+  final IconData iconImage;
+
+  Category({@required this.type, @required this.iconImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        iconImage,
+        size: 30.0,
+      ),
+      title: Text("$type"),
+    );
   }
 }
